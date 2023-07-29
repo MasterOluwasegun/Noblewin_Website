c6560cca4fb7eec9140eb6c91afed89e4fff5f85
Sdcabasa <sdcabasa@gmail.com>
2023-07-29 06:08:02 +0100

    First Uploaded

---
+++ Noblewin_Website.js
@@ -0,0 +1,9 @@
+// -------- JS for toggle menu --------
+
+let nav_links = document.getElementById("nav_links");
+        function openmenu(){
+            nav_links.style.right = "0";
+        }
+        function closemenu(){
+            nav_links.style.right = "-200px";
+        }
